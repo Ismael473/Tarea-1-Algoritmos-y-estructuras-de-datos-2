094f2206418a940ae911be0dd9485520663f04ae
Ismael473 <ismaelmena73@gmail.com>
2023-03-10 21:03:27 -0600

    Solucion de errores

---
+++ tarea1/cmake-build-debug/CMakeFiles/tarea1.dir/Prueba.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/tarea1.dir/Prueba.cpp.obj: \
+ D:\documentos\C++\ projects\Tarea-1-Algoritmos-y-estructuras-de-datos-2\tarea1\Prueba.cpp \
+ D:\documentos\C++\ projects\Tarea-1-Algoritmos-y-estructuras-de-datos-2\tarea1\Prueba.h
